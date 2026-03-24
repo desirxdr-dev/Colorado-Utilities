663f2a479a1f769ed4c8bd49839e748f93348df5
stillrdtr <stillrdtr@gmail.com>
2026-03-23 19:45:29 -0700

    a

--- commands/prefix/taffiliations.js
+++ commands/prefix/taffiliations.js
@@ -19,6 +19,11 @@ module.exports = {
   "content": "# <:Qmark:1485398931557978213> Affiliations\nOur affiliations at <:ontariorp:1485831045554372609> **Ontario Roleplay** are handpicked by our **Directive Team**. Opening\n a ticket to affiliate may result in moderation. We will reach out to your server if we are \ninterested in affiliating with you."
 })
 
+
+    try {
+      await message.delete();
+    } catch {}
+    
     }
 
 }
