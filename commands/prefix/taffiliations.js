ff40e1d2b87ababbe6f2a2602de79dfebc322fff
stillrdtr <stillrdtr@gmail.com>
2026-03-23 19:44:47 -0700

    a

--- commands/prefix/taffiliations.js
+++ commands/prefix/taffiliations.js
@@ -16,7 +16,7 @@ module.exports = {
 
 
     await message.channel.send({
-  "content": "# <:Qmark:1485398931557978213> Affiliations\nOur affiliations at <:ontariorp:1485831045554372609> **Ontario Roleplay** are handpicked by our **Directive Team**. Opening a ticket to affiliate may result in moderation. We will reach out to your server if we are interested in affiliating with you."
+  "content": "# <:Qmark:1485398931557978213> Affiliations\nOur affiliations at <:ontariorp:1485831045554372609> **Ontario Roleplay** are handpicked by our **Directive Team**. Opening\n a ticket to affiliate may result in moderation. We will reach out to your server if we are \ninterested in affiliating with you."
 })
 
     }
